3a12c6c4600bf009fc40512af3941fea3504d28f
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2025-04-03 13:25:31 -0500

    Create 01-Valid Parentheses(#20).ts

---
+++ stack/01-Valid Parentheses(#20).ts
@@ -0,0 +1,18 @@
+function isValid(s: string): boolean {
+  const stack: string[] = [];
+  const map: Record<string, string> = {
+    ')': '(',
+    ']': '[',
+    '}': '{',
+  };
+
+  for (let char of s) {
+    if (Object.values(map).includes(char)) {
+      stack.push(char);
+    } else {
+      if (stack.pop() !== map[char]) return false;
+    }
+  }
+
+  return stack.length === 0;
+}
