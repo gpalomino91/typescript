06ffe2733394c0c304e8b93bc715332916fd3b02
Gonzalo Palomino <104773712+gpalomino91@users.noreply.github.com>
2025-04-03 13:39:09 -0500

    Create 03-Move-Zeroes(#283).ts

---
+++ two-pointers/03-Move-Zeroes(#283).ts
@@ -0,0 +1,14 @@
+function moveZeroes(nums: number[]): void {
+  let nonZeroIndex = 0;
+
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] !== 0) {
+      nums[nonZeroIndex] = nums[i];
+      nonZeroIndex++;
+    }
+  }
+
+  for (let j = nonZeroIndex; j < nums.length; j++) {
+    nums[j] = 0;
+  }
+}
